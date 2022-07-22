214f16a7bbfd1cb69b6a45287c6e4b43a8c769cb
MarjanBaghdadi <35376484+MarjanBaghdadi@users.noreply.github.com>
2022-07-22 13:01:15 +0200

    Create Book list database.sql

---
+++ Book list database.sql
@@ -0,0 +1,11 @@
+
+
+
+CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER );
+
+
+INSERT INTO books VALUES (1, "pride and prejidous", 7);
+INSERT INTO books VALUES (2, "Atomic habits", 3);
+INSERT INTO books VALUES (3, "rich dad poor dad", 5);
+
+SELECT * FROM books;
